cbfe3c0008ffb9c54c78277fa31215b4142adf74
Caleb Rogers <rogersjcaleb@gmail.com>
2019-03-12 13:46:37 -0700

    Make it so adding more buttons causes more clicks per button

--- click.js
+++ click.js
@@ -1,27 +1,46 @@
-const BUTTON = document.querySelector('button.click');
-const COUNTER = document.querySelector('.click-counter');
-let count = 0;
 const state = {
   level: 0,
+  arena_BUTTONS: [],
+  basic_increment: 1
 };
-BUTTON.onclick = ()=>{
-  count++;
+let count = 0;
+addButtonToArena(createButton('Click', 'click', CLICK_BUTTON));
+const BUTTON = document.querySelector('button.click');
+const COUNTER = document.querySelector('.click-counter');
+BUTTON.onclick = CLICK_BUTTON;
+
+function CLICK_BUTTON() {
+  incrementCount();
+}
+
+function incrementCount(){
+  count += state.basic_increment;
   COUNTER.innerHTML = count;
   stateCheck(count);
-};
+}
 
 function stateCheck(count) {
   if (state.level < 1) {
     if (count === 10) {
-      addButtonToUpgrades(createButton('Another Button'));
+      addButtonToUpgrades(createButton(
+        'Another Button',
+        'click',
+        addAnotherArenaButton
+      ));
     }
   }
 }
 
-function createButton(text, callback){
+function addAnotherArenaButton(){
+  state.basic_increment++;
+  addButtonToArena(createButton('Click', 'click', CLICK_BUTTON));
+}
+
+function createButton(text, type, callback){
   const new_button = document.createElement('button');
   new_button.innerHTML = text;
-  new_button.onclick = callback;
+  new_button.classList.add(type);
+  new_button.onclick = callback.bind(new_button);
   return new_button;
 }
 
@@ -31,4 +50,11 @@ function addButtonToUpgrades(button){
   new_upgrade_list_item.appendChild(button);
   upgrades_list.appendChild(new_upgrade_list_item);
   return upgrades_list;
+}
+
+function addButtonToArena(button){
+  const arena = document.querySelector('.arena');
+  arena.appendChild(button);
+  state.arena_BUTTONS.push(button);
+  return arena;
 }
